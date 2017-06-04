2281ef08b2a67d86bcc5ba45f4c41f5e44209334
Atsushi SHIBATA <zhengjianping@zhengjianping-no-MacBook-Air.local>
2017-06-04 21:37:18 +0900

    code change

--- test.sh
+++ test.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-echo "hello world"
+a=5
+b=40
+
+function gcd ()
+ {
+   num1=$1
+   num2=$2
+   r=1
+   until [ "$r" -eq 0 ]
+   do
+     let "r = $num1 % $num2"
+     num1=$num2
+     num2=$r
+   done
+ }
+ gcd a b
+ echo " $a and $b の最大公約数 = $num1"
