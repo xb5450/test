48262f261f6eb13f8e2c148a0c75e4602a0a2614
Atsushi SHIBATA <zhengjianping@zhengjianping-no-MacBook-Air.local>
2017-06-04 23:10:17 +0900

    change code

--- test.sh
+++ test.sh
@@ -2,17 +2,16 @@
 
 a=5
 b=40
+read -p "二つの自然数を入手してくだい：" a b
 
-function gcd {
-   num1=$1
-   num2=$2
-   r=1
-   until [ "$r" -eq 0 ]
-   do
+num1=$1
+num2=$2
+r=1
+until [ "$r" -eq 0 ]
+do
      let "r = $num1 % $num2"
      num1=$num2
      num2=$r
-   done
- }
- gcd a b
- echo " $a and $b の最大公約数 = $num1"
+done
+
+echo " $a and $b の最大公約数 = $num1"
